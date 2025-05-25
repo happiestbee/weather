0e7e9a90173bd133c403892b9584db47eba3c87f
hvars7 <149632649+hvars7@users.noreply.github.com>
2025-05-25 23:10:08 +0100

    Update theme.dart
    Changes to theme data

--- lib/data/theme.dart
+++ lib/data/theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 ThemeData getThemeForWeather(int code) {
-  if (code == 0) {
+  if (code >= 0 && code <= 2) {
     // Clear/Sunny
     return ThemeData(
       scaffoldBackgroundColor: const Color(0xFF32ADE6),
@@ -10,8 +10,17 @@ ThemeData getThemeForWeather(int code) {
         bodyMedium: TextStyle(color: Colors.black),
       ),
     );
-  } else if (code >= 61 && code <= 67) {
+  } else if ((code >= 51 && code <= 67) || (code >= 80 && code <= 82)) {
     // Rainy
+    return ThemeData(
+      scaffoldBackgroundColor: const Color.fromARGB(255, 92, 117, 145),
+      primaryColor: Color.fromARGB(255, 92, 117, 145),
+      textTheme: const TextTheme(
+        bodyMedium: TextStyle(color: Colors.black),
+      ),
+    );
+  } else if (code >= 3 && code <= 48) {
+    // Cloudy
     return ThemeData(
       scaffoldBackgroundColor: Colors.grey.shade700,
       primaryColor: Colors.grey.shade600,
@@ -19,7 +28,7 @@ ThemeData getThemeForWeather(int code) {
         bodyMedium: TextStyle(color: Colors.black),
       ),
     );
-  } else if (code >= 71 && code <= 77) {
+  } else if ((code >= 71 && code <= 77) || (code == 85 || code == 86)) {
     // Snowy
     return ThemeData(
       scaffoldBackgroundColor: const Color.fromARGB(255, 157, 171, 178),
@@ -28,11 +37,20 @@ ThemeData getThemeForWeather(int code) {
         bodyMedium: TextStyle(color: Colors.black),
       ),
     );
+  } else if (code >= 95 && code <= 99) {
+  // Cloudy
+  return ThemeData(
+    scaffoldBackgroundColor: Colors.grey.shade700,
+    primaryColor: Colors.grey.shade600,
+    textTheme: const TextTheme(
+      bodyMedium: TextStyle(color: Colors.black),
+    ),
+  );
   } else {
 
     return ThemeData(
-      scaffoldBackgroundColor: Colors.blueGrey.shade800,
-      primaryColor: Colors.blueGrey.shade700,
+      scaffoldBackgroundColor: const Color.fromARGB(255, 120, 162, 182),
+      primaryColor: const Color.fromARGB(255, 120, 162, 182),
       textTheme: const TextTheme(
         bodyMedium: TextStyle(color: Colors.black),
       ),
